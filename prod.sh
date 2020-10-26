f1a621ec0731dbfa17a42ad4469776698e66f97a
Chipster <julien.chipster@gmail.com>
2020-10-26 14:19:39 +0100

    Change version number

--- prod.sh
+++ prod.sh
@@ -1,4 +1,4 @@
 # Création de l'image
-docker build -t img_alpine_rpi_camalarm:v1.0.2 .
+docker build -t img_alpine_rpi_camalarm:v1.0.3 .
 # Création du container
-docker run -d --restart unless-stopped --device /dev/vchiq --name server_alpine_rpi_camalarm-v1.0.2 -p 8080:8080 -p 8081:8081 -p 8090:8090 img_alpine_rpi_camalarm:v1.0.2
+docker run -d --restart unless-stopped --device /dev/vchiq --name server_alpine_rpi_camalarm-v1.0.3 -p 8080:8080 -p 8081:8081 -p 8090:8090 img_alpine_rpi_camalarm:v1.0.3
