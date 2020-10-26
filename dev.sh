0b874c8b99bbb52cfb7911ac62aaf678d35e4876
Chipster <julien.chipster@gmail.com>
2020-10-26 14:21:10 +0100

    add -ti options to solve a crash a start

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 # Création de l'image
 docker build -f Dockerfile_dev -t img_server_rpi_camalarm:dev .
 # Création du container
-docker run -d --restart unless-stopped --device /dev/vchiq --name server_rpi_camalarm-dev -p 8080:8080 -p 8081:8081 -p 8090:8090 img_server_rpi_camalarm:dev
+docker run -tid --restart unless-stopped --device /dev/vchiq --name server_rpi_camalarm-dev -p 8080:8080 -p 8081:8081 -p 8090:8090 img_server_rpi_camalarm:dev
